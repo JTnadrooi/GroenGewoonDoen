041a8307e76e59b1d4b07207f41dd351effbafa2
Element51Sb <sybbgaming@gmail.com>
2026-02-23 13:00:06 +0100

    Update npm scripts and docs

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Project: Groen & Gewoon Doen
 
 Website & Beheersysteem voor een Hovenier
+
+## Install & Run
+```bash
+npm install
+npm start
+
+## For auto restart
+npm run dev
+
+## Run this to test if it works, it should return "Ahoy!"
+http://localhost:3000/message
--- js/server.js
+++ js/server.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.get('/message', (req, res) => {
-    res.send('Ahoy!');
+app.use(express.json());
+
+// Serve static folders (so /css, /js, /media, /html work)
+app.use(express.static(path.join(__dirname, "..")));
+
+// Homepage -> your html/index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "html", "index.html"));
 });
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+// Existing test route
+app.get("/message", (req, res) => {
+  res.send("Ahoy!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
